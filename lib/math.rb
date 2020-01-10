f22f91d36791dc2d89efe9d831f418605887fe25
Ha Nguyen <nkei1908@yahoo.com>
2020-01-10 05:18:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num2 / num1
+  num1 / num2
 end
 
 def multiplication(num1, num2)
@@ -15,9 +15,9 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num2 % num1
+  num1 % num2
 end
 
 def square_root(num)
-  Math.square_root(num)
+  return Math.square_root(num)
 end
